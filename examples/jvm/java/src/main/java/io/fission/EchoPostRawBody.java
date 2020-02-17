dbd7e83f34ff3079178e982a949f7c10aa3ac10f
Harjinder Dhanjal <malotian@gmail.com>
2020-02-17 17:49:50 +0000

    rename

--- examples/jvm/java/src/main/java/io/fission/EchoPostRawBody.java
+++ examples/jvm/java/src/main/java/io/fission/EchoPostRawBody.java
@@ -5,7 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 
-public class EchoRawBody implements Function {
+import io.fission.Context;
+import io.fission.Function;
+
+public class EchoPostRawBody implements Function {
 
 	@Override
 	public ResponseEntity<?> call(RequestEntity req, Context context) {
@@ -13,3 +16,4 @@ public class EchoRawBody implements Function {
 	}
 
 }
+
